ed92d5daac9287b0eb70af54081e6b328e9cec04
BintangKantata <52192720+BintangKantata@users.noreply.github.com>
2025-06-01 17:16:52 +0700

    Update ToDoListApplication.java

--- ToDoList/ToDoList/src/main/java/com/pboToDoList/ToDoList/ToDoListApplication.java
+++ ToDoList/ToDoList/src/main/java/com/pboToDoList/ToDoList/ToDoListApplication.java
@@ -2,11 +2,12 @@ package com.pboToDoList.ToDoList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ToDoListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
 }
-
