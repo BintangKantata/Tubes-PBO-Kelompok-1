3bbc44d8fa7775dba8cc66486c043373d867c6f2
Azzamy <145249568+azzamybil@users.noreply.github.com>
2025-06-02 22:00:15 +0700

    Update TaskRepository.java

--- ToDoList/ToDoList/src/main/java/com/pboToDoList/ToDoList/repository/TaskRepository.java
+++ ToDoList/ToDoList/src/main/java/com/pboToDoList/ToDoList/repository/TaskRepository.java
@@ -4,12 +4,26 @@ import com.pboToDoList.ToDoList.task.Task;
 import com.pboToDoList.ToDoList.task.TaskPriority;
 import com.pboToDoList.ToDoList.user.RegularUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
     List<Task> findByRuser(RegularUser ruser);
+    @Query("SELECT t FROM Task t WHERE " +
+            "t.ruser = :ruser AND " +
+            "(:title IS NULL OR LOWER(t.title) LIKE LOWER(CONCAT('%', :title, '%'))) AND " +
+            "(:categoryId IS NULL OR t.category.id = :categoryId) AND " +
+            "(:priority IS NULL OR t.priority = :priority) AND " +
+            "(:completed IS NULL OR t.completed = :completed)")
+    List<Task> findByFilters(@Param("title") String title,
+                             @Param("categoryId") Integer categoryId,
+                             @Param("priority") TaskPriority priority,
+                             @Param("completed") Boolean completed,
+                             @Param("ruser") RegularUser ruser);
+    List<Task> findByDeadline(LocalDate deadline);
 }
-
